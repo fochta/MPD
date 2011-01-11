c3cfc1c5bb47e380ce8f2a718223472fb0b46f21
Adam Focht <fochta@dm.org>
2011-01-11 12:08:52 -0500

    Base Rails/PG App

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mpd::Application.config.secret_token = '987a5b024dfe806f470c55cc1f1e9094d8f172c8dfefe349fd6e6289adccb0bea0f833b06aed7e8557e61c0998ec139f2bf5fc1df2fc05d8bbc9fa319605678f'
